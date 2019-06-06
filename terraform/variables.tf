dca994fcc108e12c5ab1676804d47b354bb38359
TrejGun <trejgun@gmail.com>
2019-06-06 11:09:55 +0400

    ecs

---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "eu-west-1"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "852196116519.dkr.ecr.eu-west-1.amazonaws.com/first:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 3000
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 3
+}
+
+variable "health_check_path" {
+  default = "/ping"
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "1024"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "2048"
+}
